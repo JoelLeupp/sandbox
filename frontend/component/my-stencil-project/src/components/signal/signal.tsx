f56d65d421fc26303bb4dc07c849b578df626baf
Joel Leupp <joelleupp@gmail.com>
2024-03-15 17:51:09 +0100

    formatting

--- frontend/component/my-stencil-project/src/components/signal/signal.tsx
+++ frontend/component/my-stencil-project/src/components/signal/signal.tsx
@@ -43,32 +43,32 @@ export class Signal {
       }
     }
 
-    private getSignalFill(status:Status, target: Status): string {
-      return status === target ? this.getColor(status) : 'white';
-    }
-
-    render() {
-      const signalCircles: SignalCircles[] =
-      [{cx: 7, color: 'red' , signal:"negative"},
-      {cx: 20,  color: 'yellow',signal:"neutral"},
-      {cx: 33,  color: 'green',signal:"positive"}]
-
-    return (
-      <div>
-        <svg viewBox="0 -5 40 10" xmlns="http://www.w3.org/2000/svg">
-          {signalCircles.map(item => (
-            <circle
-              cx={item.cx}
-              cy="0"
-              r="4.5"
-              stroke-width="0.25"
-              stroke={item.color}
-              fill={this.getSignalFill(this.status, item.signal)}
-              onClick={() => this.setStatus(item.signal)}
-            />
-          ))}
-        </svg>
-      </div>
-    );
+  private getSignalFill(status:Status, target: Status): string {
+    return status === target ? this.getColor(status) : 'white';
   }
+
+  render() {
+    const signalCircles: SignalCircles[] =
+    [{cx: 7, color: 'red' , signal:"negative"},
+    {cx: 20,  color: 'yellow',signal:"neutral"},
+    {cx: 33,  color: 'green',signal:"positive"}]
+
+  return (
+    <div>
+      <svg viewBox="0 -5 40 10" xmlns="http://www.w3.org/2000/svg">
+        {signalCircles.map(item => (
+          <circle
+            cx={item.cx}
+            cy="0"
+            r="4.5"
+            stroke-width="0.25"
+            stroke={item.color}
+            fill={this.getSignalFill(this.status, item.signal)}
+            onClick={() => this.setStatus(item.signal)}
+          />
+        ))}
+      </svg>
+    </div>
+  );
+}
 }
