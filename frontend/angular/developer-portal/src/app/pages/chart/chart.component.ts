24b132c4b3755faee4ba7e5fdf546846995f2d22
Joel Leupp <joelleupp@gmail.com>
2024-06-03 14:35:10 +0200

    chart page

--- frontend/angular/developer-portal/src/app/pages/chart/chart.component.ts
+++ frontend/angular/developer-portal/src/app/pages/chart/chart.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
 import { Food } from '@api/api.models';
 import { Store } from '@ngrx/store';
 import { UiLibraryAngularModule } from '@six-group/ui-library-angular';
@@ -11,12 +11,15 @@ import {
   selectSelectedFoodItem,
 } from 'src/app/stores/food/food.reducer';
 import { LetDirective } from '@ngrx/component';
+import * as PlotlyJS from 'plotly.js-dist-min';
+import { PlotlyModule } from 'angular-plotly.js';
 
+PlotlyModule.plotlyjs = PlotlyJS;
 
 @Component({
   selector: 'app-chart',
   standalone: true,
-  imports: [CommonModule, UiLibraryAngularModule, LetDirective],
+  imports: [CommonModule, UiLibraryAngularModule, LetDirective, PlotlyModule],
   templateUrl: './chart.component.html',
   styleUrl: './chart.component.scss',
 })
@@ -27,6 +30,9 @@ export class ChartComponent implements OnInit {
     this.store.dispatch(FoodListActions.loadFood());
     // this.store.select(selectFoodList).subscribe(foodList => this.foodList = foodList)
     // this.store.select(selectSelectedFood).subscribe(foodName => this.selectedFood = foodName)
+    this.store
+      .select(selectSelectedFoodItem)
+      .subscribe((foodInfo) => this.foodInfo.set(foodInfo));
   }
 
   selectedFood$ = this.store.select(selectSelectedFood);
@@ -36,4 +42,68 @@ export class ChartComponent implements OnInit {
   selectFood(name: string | string[]) {
     this.store.dispatch(FoodListActions.selectFood({ name }));
   }
+
+  foodInfo = signal<Food | null>(null);
+
+  graphLayout = computed(() => {
+    return {
+      autosize: true,
+      ...(this.foodInfo() && {
+        title: {
+          text: `<b>${
+            this.foodInfo()?.name
+          }</b><br><span style="font-size:.85em">Calories: ${
+            this.foodInfo()?.calories
+          }</span>`,
+          font: {
+            size: 18,
+          },
+          yref: 'container',
+          xanchor: 'left',
+          yanchor: 'top',
+          x: 0,
+          y: 0.9,
+        },
+      }),
+      xaxis: {
+        tickwidth: 1,
+      },
+      yaxis: {
+        showgrid: true,
+        zerolinewidth: 2,
+        ticksuffix: '  ',
+      },
+    };
+  });
+
+  graphData = computed(() => {
+    return [
+      {
+        name: 'Nutrition',
+        x: ['Fat', 'Carbs', 'Protein'],
+        y: [
+          this.foodInfo()?.fat,
+          this.foodInfo()?.carbs,
+          this.foodInfo()?.protein,
+        ],
+        type: 'bar',
+        marker: {
+          color: '#373a36',
+        },
+        hovertemplate: '%{label}: %{value:.1f}g',
+        cliponaxis: false,
+      },
+    ];
+  });
+
+  graph = computed(() => {
+    return {
+      data: this.graphData(),
+      layout: this.graphLayout(),
+      config: {
+        displayModeBar: false,
+      },
+      style: { position: 'relative', width: '100%', height: '100%' },
+    };
+  });
 }
